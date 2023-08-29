4ef90ceaf72389f2bd929288e664f20abd6565b2
banahaker <pn0818x@outlook.com>
2023-08-29 20:25:08 +0800

    feat: background color of candidate list

--- app/components/dash.tsx
+++ app/components/dash.tsx
@@ -19,7 +19,7 @@ export default function Dash({ list }: IProp) {
           src="/images/lei.png"
           width={200}
         />
-        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/50">
+        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/75">
           <p className="text-lg">賴清德</p>
           <p className="text-4xl font-bold">
             {list.filter((i) => i.candidate == 0).length}
@@ -35,7 +35,7 @@ export default function Dash({ list }: IProp) {
           src="/images/huo.png"
           width={200}
         />
-        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/50">
+        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/75">
           <p className="text-lg">侯友宜</p>
           <p className="text-4xl font-bold">
             {list.filter((i) => i.candidate == 1).length}
@@ -51,7 +51,7 @@ export default function Dash({ list }: IProp) {
           src="/images/ke.png"
           width={200}
         />
-        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/50">
+        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/75">
           <p className="text-lg">柯文哲</p>
           <p className="text-4xl font-bold">
             {list.filter((i) => i.candidate == 2).length}
@@ -67,7 +67,7 @@ export default function Dash({ list }: IProp) {
           src="/images/guo.png"
           width={200}
         />
-        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/50">
+        <CardFooter className="justify-center overflow-hidden py-1 absolute before:rounded-xl rounded-large w-[calc(100%_-_20px)] h-[calc(100%_-_20px)] ml-[10px] mt-[10px] z-10 flex flex-col bg-gray-500/75">
           <p className="text-lg">郭台銘</p>
           <p className="text-4xl font-bold">
             {list.filter((i) => i.candidate == 3).length}
